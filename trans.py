370ed7a66d935900f5e78647e72510fe76cf9ca5
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2019-12-20 22:43:43 +0100

    Added upper casse functions and changed variable names

--- trans.py
+++ trans.py
@@ -24,7 +24,7 @@
 # else: 
 #     print("Enter something resonable")
 while True:
-    igboDict = {'come': 'bia', 
+    igboDict = {'come': 'Bia', 
                 'welcome':'Nnọọ', 
                 'welcome':'Dalụ', 
                 'How are you':'Kedụ',
@@ -44,29 +44,31 @@ while True:
 
     choice = input("Press 1 for Eng-Igbo and 2 for Igbo-Eng: ")
     if choice == "1":
-        j= input ("Enter a an English word: ")
+        j = input ("Enter an English word or phrase: ")
         word = igboDict.get(j)
+        newJ = j[0:1].upper() + j[1:].lower()
         if word:
-            print(f'{j} in igbo means {word}')
+            print(f'{newJ} in igbo means {word}')
         else:
-            print(f'{j} cannot be found here')
+            print(f'{newJ} cannot be found here')
     elif choice == "2":
-        j= input ("Enter an Igbo word: ")
+        j= input ("Enter an Igbo word or phrase: ")
         word = engDict.get(j)
+        newJ = j[0:1].upper() + j[1:].lower()
         if word:
-            print(f'{j} in english means {word}')
+            print(f'{newJ} in English means {word}')
         else:
-            print(f'{j} cannot be found here')    
+            print(f'{newJ} cannot be found in dictionary')    
     else: 
         print("Your choice is not valid")
 
     while True:
-        answer = input('Do you want to make another translation? (y/n): ')
+        answer = input('Do you want to translate another word or phrase? (y/n): ')
         if answer in ('y', 'n'):
             break
         print('Invalid input.')
     if answer == 'y':
         continue
     else:
-        print ('Goodbye')
+        print ('Thank you!')
         break
